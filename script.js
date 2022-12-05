ad55f4dac6c19bb25ff9cb56339987f038538e13
Chase <98079776+chasevriend@users.noreply.github.com>
2022-12-05 08:22:36 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const num = [
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+];
+
+function add(num1, num2) {
+    let sum = num1 + num2;
+};
+
+function subtract(num1, num2) {
+    let sum = num1 + num2;
+};
+
+function multiply(num1, num2) {
+    let sum = num1 * num2;
+};
+
+function divide(num1, num2) {
+    let sum = num1 / num2;
+};
